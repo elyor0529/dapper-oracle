38de093a1480e7861403a6333945bd138c260200
Vladislav <vladislav@wallit.eu>
2020-11-27 17:23:12 +0500

    create demo app

---
+++ UzEx.Dapper.Oracle.Demom/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Oracle.ManagedDataAccess.Client;
+
+namespace UzEx.Dapper.Oracle.Demo
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var conn=new OracleConnection();
+            var com = conn.CreateCommand();
+            
+            com.Parameters.Add(new OracleDynamicParameters());
+
+            Console.WriteLine("Done!");
+        }
+    }
+}
